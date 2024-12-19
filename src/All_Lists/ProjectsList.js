e14236a21f0ccb1a51cc03a51d8d535ef4edab88
razzle.div <barlisanrussell@gmail.com>
2024-12-19 22:15:44 +0800

    second commit

--- src/All_Lists/ProjectsList.js
+++ src/All_Lists/ProjectsList.js
@@ -8,28 +8,24 @@ const ProjectsList = [
         id: 2,
         title: 'Current Weather',
         link: 'https://weather-webapp-pro.vercel.app',
-        github: 'https://github.com/chiragjain307/Weather-WebApp',
         image: img2
     },
     {
         id: 3,
         title: 'Social Media Blogging App',
         link: 'https://bloging-webapp.vercel.app',
-        github: 'https://github.com/chiragjain307/appWrite_blog_app',
         image: img3
     },
     {
         id: 4,
         title: 'Tours & Travel',
         link: 'https://tours-travel-pro.vercel.app',
-        github: 'https://github.com/chiragjain307/Tours-Travel',
         image: img4
     },
     {
         id: 5,
         title: 'Media Player',
         link: 'https://media-player-pro.vercel.app',
-        github: 'https://github.com/chiragjain307/Media-Player',
         image: img5
     },
     
